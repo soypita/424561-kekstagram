61f386be2e26b0b3f30b475d2da56743b4a69156
Dmitry <soyaburritos@gmail.com>
2017-11-23 18:46:12 +0300

    fix

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,17 @@
 'use strict';
 
+var NUMBER_OF_PICTURES = 24;
+var MIN_COUNT_OF_LIKES = 15;
+var MAX_COUNT_OF_LIKES = 200;
+var USER_COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
 var getRandomInRange = function (min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 };
@@ -13,22 +25,10 @@ var getRandomComments = function () {
   return outCommentsArr;
 };
 
-var NUMBER_OF_PICTURES = 24;
-var MIN_COUNT_OF_LIKES = 15;
-var MAX_COUNT_OF_LIKES = 200;
-var USER_COMMENTS = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
 var pictures = [];
 
 for (var i = 0; i <= NUMBER_OF_PICTURES; i++) {
-  var urlPath = 'photos/{{' + (i + 1) + '}}.jpg';
+  var urlPath = 'photos/' + (i + 1) + '.jpg';
   var likesCounter = getRandomInRange(MIN_COUNT_OF_LIKES, MAX_COUNT_OF_LIKES);
   var userComments = getRandomComments();
   pictures[i] =

9098d319fee9d947afd10926734ef7e6a02db788
Dmitry <soyaburritos@gmail.com>
2017-12-07 22:18:53 +0300

    Универсальный контрол фильтра

--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -1,13 +1,11 @@
 'use strict';
 
 (function () {
-  var DEFAULT_EFFECT_LEVEL = 20;
-
   window.initializeFilters = function (filterElement, cb) {
     filterElement.addEventListener('click', function (evt) {
       if (evt.target.type === 'radio') {
         var filterName = evt.target.value;
-        cb(filterName, DEFAULT_EFFECT_LEVEL);
+        cb(filterName);
       }
     });
   };
